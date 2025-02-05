1d718b215921ac514252a36737bb39f8d6367810
Jojamma <jojamma11@gmail.com>
2025-02-05 13:24:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,7 +128,7 @@ else:
 
         elif page == "Log Page":
             st.title("Log Page")
-            logs_df = pd.read_sql("SELECT * FROM logs ORDER BY timestamp DESC", conn)
+            logs_df = pd.read_sql("SELECT * FROM logs WHERE username = ? ORDER BY timestamp DESC", conn, params=(st.session_state.username,))
             if not logs_df.empty:
                 logs_df.drop(columns=["id"], inplace=True)
                 logs_df.set_index("timestamp", inplace=True)
